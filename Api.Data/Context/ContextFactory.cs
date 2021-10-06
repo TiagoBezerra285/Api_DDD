8baab54e9bd15d9cffe161002c8b220c5e043d2f
tiagobezerra285 <tiagobezerra285@gmail.com>
2021-10-05 23:59:54 -0300

    Adicionar arquivos de projeto.

---
+++ Api.Data/Context/ContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Context
+{
+    class ContextFactory : IDesignTimeDbContextFactory<MyContext>
+    {
+        public MyContext CreateDbContext(string[] args)
+        {
+            //usado para Criar as Migrações
+            var connectionString = "Server=localhost;Port=3306;Database=ddbApi_DDD;Uid=root;Pwd123456";
+            var optionsBuilder = new DbContextOptionsBuilder<MyContext> ();
+            optionsBuilder.UseMySql(connectionString);
+            return new MyContext(optionsBuilder.Options);
+
+         
+        }
+    }
+}
